2a6675fc3899ec42611b0f0ef1fa9387c3225866
saivarun-06 <saivarun5262@gamil.com>
2023-08-21 12:19:49 +0530

    JAVAOOPS

---
+++ Polymorphism/src/Polymorphism/Poly1.java
@@ -0,0 +1,8 @@
+package Polymorphism;
+public class Poly1 
+{
+	public static void main(String[] args) 
+	{
+		
+	}																																						
+}
---
+++ Polymorphism/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Raghuvarun
+ *
+ */
+module Polymorphism {
+}
